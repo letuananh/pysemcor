60fbd4fec607c01a79e4184fbe74705917ea414e
Le Tuan Anh <tuananh.ke@gmail.com>
2014-12-02 23:33:57 +0700

    Add Semcor 3.0 (Rada) and a project structure.

---
+++ data/README.md
@@ -0,0 +1,2 @@
+3rada: Semcor 3.0 (Mapped to Princeton WordNet 3.0) by Rada Mihalcea <rada@cs.unt.edu>, University of North Texas (June 13, 2008)
+
---
+++ pysemcor/model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+	print("Semcor-models is a library, not an application.")
+	pass
+
+if __name__ == "__main__":
+	main()
